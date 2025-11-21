9a3739437b99af3f8e73e8615e41ad30919e3873
NamNoria <jt763469905@gmail.com>
2025-11-21 10:49:43 +0800

    删除非EDR项目

--- TestFramework/main.mm
+++ TestFramework/main.mm
@@ -1,6 +1,5 @@
 #import <Cocoa/Cocoa.h>
 #include "CThreatDetect.h"
-#import <GetHardwareInfo/HardwareInfo.h>
 #include <string>
 
 //void cleanup() { NSLog(@"atexit cleanup called"); }
@@ -9,12 +8,6 @@ int main(int argc, const char *argv[])
 {
     @autoreleasepool
     {
-        
-        // 先获取并打印一次硬件信息
-//        HardwareInfo hw;
-//        std::string hwInfo = hw.GetHardwareInfo(nullptr);
-//        NSLog(@"Hardware Info:\n%.*s", (int)hwInfo.size(), hwInfo.c_str());
-         
         
         CThreatDetect *detector = CThreatDetect::Shared();
         if (detector)
